9d0bb27d9e55e80aa8f147f6a3978b6880ed0cf8
SarayMF <57024681+SarayMF@users.noreply.github.com>
2022-07-19 22:27:38 -0500

    [EDIT] Configuraciones
    Cambiar las configuraciones de ruta en app y env

--- app/Config/App.php
+++ app/Config/App.php
@@ -24,7 +24,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost/ResidenciasSaray/plataforma/public';
+    public $baseURL = 'http://localhost/residencias/public/';
 
     /**
      * --------------------------------------------------------------------------
